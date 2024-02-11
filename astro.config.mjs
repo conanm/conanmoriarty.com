4f2ab7a3c88b967d2308ecd8da7d7958bb0f7043
cdm <conanm@gmail.com>
2024-02-11 16:34:14 +0100

    feat: add GA.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://www.conanmoriarty.com',
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
+        }),
+        partytown({
+            config: {
+                forward: ['dataLayer.push']
+            }
         })
     ]
 });
